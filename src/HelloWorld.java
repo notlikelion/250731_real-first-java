5c1b2e98d0175ac2f573e40b432be330d2c07c7c
notlikelion <notlikelion@gmail.com>
2025-07-31 11:03:54 +0900

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    // 진입점 : 이 Java 파일을 실행했을 때 이 구문을 찾아서 실행시킴
+    // main
+    public static void main(String[] args) {
+        // sout
+        // System.out.println();
+        // System.out.println("{본인이 출력하고자 하는 내용}");
+        // 쌍따옴표 주의!!! (작은따옴표 안돼요!)
+        System.out.println("Hello World!");
+        // 같은 줄 복사 Ctrl + D / cmd + D
+        System.out.println("Bye Earth");
+        // cmd + delete, ctrl + y
+        // 약간의 중간 팁? : 본인 OS에 맞는 IntellJ 주요 단축키 찾아보기
+        // https://g.co/gemini/share/37171052db38
+    }
+}
