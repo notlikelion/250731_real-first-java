e46770ceca428a4d9c3fde30b80725b2b4e2fdac
notlikelion <notlikelion@gmail.com>
2025-07-31 11:49:42 +0900

    Gemini

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -13,5 +13,6 @@ public class HelloWorld {
         // 약간의 중간 팁? : 본인 OS에 맞는 IntellJ 주요 단축키 찾아보기
         // https://g.co/gemini/share/37171052db38
         System.out.println("마아아아아아악");
+        // 범위 지정 -> 우클릭 -> Gemini -> ...
     }
 }
